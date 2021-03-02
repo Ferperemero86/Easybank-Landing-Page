37985c687d9e68dae2dffb0bbe06ddf224f015f8
Fernando Perez <fernandoperezmerono@gmail.com>
2021-03-02 12:03:08 +0000

    Add menu hamburger animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+
+//Hamburger animation
+const btnHamburger = document.querySelector("#btn-hamburger");
+
+btnHamburger.addEventListener("click", function() {
+    if (btnHamburger.classList.contains("open")) {
+        btnHamburger.classList.remove("open");
+    } 
+    else {
+        btnHamburger.classList.add("open");
+    }
+}) 
